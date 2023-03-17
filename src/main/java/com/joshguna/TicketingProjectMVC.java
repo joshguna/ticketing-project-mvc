02ff6aa8d79ebdf2ccf5ec5c93a7caaaeae17b20
Joshgun <joshgunf@gmail.com>
2023-03-17 18:28:39 -0400

    Initial commit, added static resources, dependencies and bootstrap

---
+++ src/main/java/com/joshguna/TicketingProjectMVC.java
@@ -0,0 +1,13 @@
+package com.joshguna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectMVC {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectMVC.class, args);
+    }
+
+}
