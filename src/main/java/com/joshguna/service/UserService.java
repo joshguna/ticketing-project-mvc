10576f6405610de9ed66d3aeaded5dd82c248703
Joshgun <joshgunf@gmail.com>
2023-04-09 15:12:12 -0400

    Manager dropdown fixed. Now at project creation we can only see managers.

--- src/main/java/com/joshguna/service/UserService.java
+++ src/main/java/com/joshguna/service/UserService.java
@@ -2,10 +2,14 @@ package com.joshguna.service;
 
 import com.joshguna.dto.UserDTO;
 
+import java.util.List;
+
 //This class is used to remove code redundancy
 public interface UserService extends CrudService<UserDTO, String> {
 
     //Extends everything in CrudService, also can have specific business logic
     //UserDTO myLogic(UserDTO userDTO);
+
+    List<UserDTO> findManagers();
 
 }
--- src/main/java/com/joshguna/service/impl/UserServiceImpl.java
+++ src/main/java/com/joshguna/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.joshguna.service.UserService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl extends AbstractMapService<UserDTO, String> implements UserService {
@@ -32,5 +33,14 @@ public class UserServiceImpl extends AbstractMapService<UserDTO, String> impleme
     @Override
     public void update(UserDTO object) {
         super.update(object.getUserName(), object);
+    }
+
+    //Code below added for listing managers in Project Creation page
+    //2 means manager
+    @Override
+    public List<UserDTO> findManagers() {
+        return super.findAll().stream()
+                .filter(userDTO -> userDTO.getRole().getId() == 2)
+                .collect(Collectors.toList());
     }
 }
