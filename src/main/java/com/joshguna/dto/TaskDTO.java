e1ab0f31d5d0e7c8603f7adace3936b9a3c6f47c
Joshgun <joshgunf@gmail.com>
2023-04-09 16:48:04 -0400

    Task functionality started. Implemented controller and updated left bar fragment

---
+++ src/main/java/com/joshguna/dto/TaskDTO.java
@@ -0,0 +1,24 @@
+package com.joshguna.dto;
+
+import com.joshguna.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskDTO {
+
+    //Long ID used for unique ID to grab info from this DTO
+    private Long id;
+    private ProjectDTO project;
+    private UserDTO assignedEmployee;
+    private String taskSubject;
+    private String taskDetail;
+    private Status taskStatus;
+    private LocalDate assignedDate;
+
+}
