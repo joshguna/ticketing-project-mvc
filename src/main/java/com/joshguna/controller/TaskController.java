e1a504bb3cea99bb2cc2c553469842714825f82a
Joshgun <joshgunf@gmail.com>
2023-04-09 23:22:34 -0400

    Fixed bugs for task update functionality

--- src/main/java/com/joshguna/controller/TaskController.java
+++ src/main/java/com/joshguna/controller/TaskController.java
@@ -49,7 +49,6 @@ public class TaskController {
         return "redirect:/task/create";
     }
 
-
     @GetMapping("/update/{taskId}")
     public String editTask(@PathVariable("taskId") Long taskId, Model model) {
 
@@ -62,19 +61,11 @@ public class TaskController {
 
     }
 
-//    @PostMapping("/update/{taskId}")
-//    public String updateTask(@PathVariable("taskId") Long taskId, TaskDTO task) {
-//        task.setId(taskId);
-//        taskService.update(task);
-//        return "redirect:/task/create";
-//    }
-
     @PostMapping("/update/{id}")
     public String updateTask(TaskDTO task) {
         taskService.update(task);
         return "redirect:/task/create";
     }
-
 
     @GetMapping("/employee/pending-tasks")
     public String employeePendingTasks(Model model) {
